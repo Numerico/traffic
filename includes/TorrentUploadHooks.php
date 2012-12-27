275dbc3ac74ed756d5ddbcec5f51ae58e6ba3323
Numerico <webmaster@numerica.cl>
2012-12-27 19:56:25 -0300

    able to insert torrent into the db but table names must have table prefix & what's the relation with articles...

--- includes/TorrentUploadHooks.php
+++ includes/TorrentUploadHooks.php
@@ -23,6 +23,8 @@ class TorrentUploadHooks{
 	 * Save uploaded torrent to database for tracking.
 	 */
 	public static function onUploadComplete(&$image){
+		
+		global $wgUser;
 				
 		if(MimeMagic::singleton()->isMatchingExtension('torrent', $image->getLocalFile()->mime)) {
 
@@ -32,13 +34,40 @@ class TorrentUploadHooks{
 			$torrentInfo = ParseTorrent($image->getLocalFile()->getLocalRefPath());//TODO what'd happen if clustered, etc? does mwstore:// help here?
 			$announce = $torrentInfo[0];
 			$infohash = $torrentInfo[1];
-			$creationdate = $torrentInfo[2];
-			$internalname = $torrentInfo[3];
+			$creationdate = $torrentInfo[2];//TODO not used
+			$name = $torrentInfo[3];
 			$torrentsize = $torrentInfo[4];
 			$filecount = $torrentInfo[5];
 			$annlist = $torrentInfo[6];
-			$comment = $torrentInfo[7];
+			$comment = $torrentInfo[7];//TODO not used
 			$filelist = $torrentInfo[8];
+			
+			//arrange
+			$name = str_replace(".torrent","",$name);
+			$name = str_replace("_", " ", $name);
+			$fname = $image->getTitle()->getBaseText();
+
+			//data
+			$upload_torrent = array(
+				//'post_id' => ''.$upload_torrent_post_id.'',
+				//'attachment_id' => ''.$upload_torrent_image_post_id.'',
+				'info_hash' => $infohash,
+				'name' => $name,
+				'filename' => $fname,//TODO what for?
+				//'save_as' => ''.$fname.'',
+				//'category' => ''.$catid.'',
+				'size' => $torrentsize,
+				//'type' => ''.$type.'',
+				'numfiles' => $filecount,
+				'owner' => $wgUser->getId(),//TODO assuming torrent is current user's
+				//'nfo' => ''.$nfo.'',
+				'announce' => $announce,
+				//'torrentlang' => ''.$langid.''//TODO mmm
+			);
+			
+			//save
+			$dbw = wfGetDB(DB_MASTER);
+			$dbw->insert('traffic_torrents',$upload_torrent);
 			
 		}
 		return true;
