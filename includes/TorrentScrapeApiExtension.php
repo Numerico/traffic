6a54cf033460bc7230504dbc8cc73d5f8ad7c0ce
Numerico <webmaster@numerica.cl>
2012-12-28 19:09:28 -0300

    responding valid scrape (apparently)

--- includes/TorrentScrapeApiExtension.php
+++ includes/TorrentScrapeApiExtension.php
@@ -11,10 +11,52 @@ class TorrentScrape extends ApiBase{
 	}
 	
 	/**
+	 * Utility: convert hexadecimal to binary
+	 */
+	function hex2bin($hexdata) {
+	  $bindata = "";
+	  for ($i=0;$i<strlen($hexdata);$i+=2) {
+	    $bindata.=chr(hexdec(substr($hexdata,$i,2)));
+	  }
+	
+	  return $bindata;
+	}
+	
+	/**
 	 * Main function
+	 * TODO lacks validations eg. infohash exists, has data in torrent_announce, etc.
 	 */
 	public function execute() {
-		$this->getResult()->addValue(null,null,'example');//call only once
+		
+		$result="d5:filesd";
+		
+			$params = $this->extractRequestParams();
+			
+			//TODO assuming a torrent can have only one infohash, while trader uses a loop...
+			$infohash = $params['infohash'];
+			$dbw = wfGetDB(DB_SLAVE);
+			$query = $dbw->select(
+				array("trr"=>"traffic_torrents","annc"=>"traffic_announce"),
+				array("trr.id","trr.filename","annc.seeders","annc.leechers","annc.times_completed"),
+				array("trr.info_hash='".$infohash."'","trr.id=annc.torrent")
+			);
+			$obj=$query->fetchObject();
+			$seeders=$obj->seeders;
+			$leechers=$obj->leechers;
+			$times_completed=$obj->times_completed;
+			$filename=$obj->filename;
+			
+			$hash = hex2bin($infohash);
+		    $result.="20:".$hash."d";
+		    $result.="8:completei".$seeders."e";
+		    $result.="10:downloadedi".$times_completed."e";
+		    $result.="10:incompletei".$leechers."e";
+		    $result.="4:name".strlen($filename).":".$filename."e";
+		    $result.="e";
+			
+		$result.="ee";
+		
+		$this->getResult()->addValue(null,null,$result);//call only once
 		return true;
 	}
 	
@@ -33,13 +75,13 @@ class TorrentScrape extends ApiBase{
     }
 	
 	/**
-	 * Params TODO
+	 * Params
 	 */
 	public function getAllowedParams() {
             return array(
-                    'param1' => array (
+                    'infohash' => array (
                             ApiBase::PARAM_TYPE => 'string',
-                            ApiBase::PARAM_REQUIRED => false
+                            ApiBase::PARAM_REQUIRED => true
                     ),
             );
     }
@@ -48,7 +90,7 @@ class TorrentScrape extends ApiBase{
 	 * Describe the parameter TODO
 	 */
     public function getParamDescription() {
-            return array( 'param1' => "nuthin\' dude..");
+            return array( 'infohash' => "infohash dude..");
     }
 	
 	/**
@@ -56,7 +98,7 @@ class TorrentScrape extends ApiBase{
 	 */
 	public function getExamples() {
     	return array(
-            'Get a sideways look (and the usual predictions)'=>' api.php?action=scrape&face=O_o&format=xml' 
+            'this is a valid torrent'=>'api.php?action=scrape&infohash=5c8f40d95f27a2fff7e763c1126f1872f7df8ed3' 
 		);
     }
 }
